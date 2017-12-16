8ff78a3ddbe00c3f459bca0db2c2d3d9d18ec6dd
VarmaSANJAY <sanjay.varma@relevancelab.com>
2017-12-16 09:40:21 +0530

    Emit new message and create new messsage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect',function () {
     console.log('Connected to server');
 
-
-    socket.emit('createMessage',{
-        from: 'ssaj@example.com',
-        text: 'Hey, This is from client plz store it.'
-    });
 });
 
 socket.on('disconnect',function () {
