a5f67bc346d1c2a5a7015c18e39ae8964a05849c
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-01-14 21:33:38 +0530

    Format timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+
+//Unix epic
+//Jan Ist 1970 00:00:00 am UTC(Time zone independent)
+ // var date = new Date();
+ // console.log(date.getMonth());
+
+
+var createdAt ;
+var date = moment();
+console.log(date.format('h:mm a'))
